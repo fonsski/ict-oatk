11af2d61e0303751ec24ec1f9a37cbc19ad1dfb6
fonsski <afonintens@gmail.com>
2025-08-27 13:28:57 +0600

    fix bugs and some imporvments

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,10 +31,7 @@
                     </label>
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg class="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                <circle cx="12" cy="7" r="4"></circle>
-                            </svg>
+                            <!-- User icon removed -->
                         </div>
                         <input type="text"
                                name="name"
--- resources/views/homepage-faq/show.blade.php
+++ resources/views/homepage-faq/show.blade.php
@@ -78,10 +78,7 @@
 
                 @if($faq->author)
                     <div class="flex items-center">
-                        <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                            <circle cx="12" cy="7" r="4"></circle>
-                        </svg>
+                        <!-- User icon removed -->
                         {{ $faq->author->name }}
                     </div>
                 @endif
